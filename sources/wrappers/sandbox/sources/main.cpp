9c8e9ccd1e9d2043de2a02087eb0d906becdd519
Piotr <mail.piotr.kulbaka@gmail.com>
2025-04-18 20:01:57 +0300

    added timer, fixed logger

--- sources/wrappers/sandbox/sources/main.cpp
+++ sources/wrappers/sandbox/sources/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
 #include <tavros/core/prelude.hpp>
+#include <tavros/core/timer.hpp>
+
+#include <inttypes.h>
 
 int main()
 {
@@ -10,12 +13,23 @@ int main()
         TAV_ASSERT(tag.data());
         std::cout << msg << std::endl;
     });
-    tavros::core::logger logger("main");
 
+    tavros::core::timer  t;
+    tavros::core::logger logger("main");
+    uint64_t             us = t.elapsed<std::chrono::nanoseconds>();
+
+    auto n = t.elapsed<std::chrono::nanoseconds>();
+    auto n2 = t.elapsed<std::chrono::nanoseconds>();
+    auto r = n2 - n;
+
+    logger.info("Elapsed time to create a logger:  %" PRIu64 " ", r);
+    t.start();
     logger.debug("Debug message");
-    logger.info("Info message");
-    logger.warning("Warning message");
-    logger.error("Error message");
+    //    logger.info("Info message");
+    //    logger.warning("Warning message");
+    //    logger.error("Error message");
+    us = t.elapsed<std::chrono::nanoseconds>();
+    logger.info("Elapsed time to write 4 messages: %" PRIu64 " ", us);
 
     return 0;
 }
