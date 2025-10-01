374cb8d4617ba9c398e26b07b885bb85210ba607
PiotrKulbaka <mail.piotr.kulbaka@gmail.com>
2025-10-01 19:41:31 +0300

    added resource_pool based on index_allocator

--- sources/libs/core/tavros/core/resources/resource_pool.hpp
+++ sources/libs/core/tavros/core/resources/resource_pool.hpp
@@ -1,96 +1,291 @@
 #pragma once
 
 #include <tavros/core/types.hpp>
-#include <tavros/core/containers/unordered_map.hpp>
-
-#include <atomic>
+#include <tavros/core/debug/assert.hpp>
+#include <tavros/core/math/bitops.hpp>
+#include <tavros/core/memory/mallocator.hpp>
+#include <tavros/core/ids/l3_bitmap_index_allocator.hpp>
 
 namespace tavros::core
 {
 
-    template<class T, class H = uint32>
+    template<class T>
     class resource_pool
     {
-    public:
-        using handle_t = H;
+    private:
+        using idx_alc_t = l3_bitmap_index_allocator;
 
     public:
-        resource_pool() = default;
-        ~resource_pool() = default;
+        using handle_t = uint32;
+
+        static constexpr handle_t invalid_handle = 0xffffffffui32;
+
+    public:
+        resource_pool()
+        {
+            std::fill(m_gen, m_gen + idx_alc_t::k_max_index, 1);
+
+            m_allocator = new mallocator();
+        }
+
+        ~resource_pool()
+        {
+            for (index_type i = 0; i <= m_max_idx; ++i) {
+                if (m_index_allocator.allocated(i)) {
+                    m_resources[i].~T();
+                }
+            }
+
+            if (m_resources) {
+                m_allocator->deallocate(reinterpret_cast<uint8*>(m_resources));
+            }
+
+            delete m_allocator;
+        }
+
+        /*struct resource_item
+        {
+            handle_t handle;
+            T& resource;
+        };
+
+        struct resource_item_iterator
+        {
+            resource_pool* pool;
+            size_t index;
+
+            bool operator!=(const iterator& other) const { return index != other.index; }
+
+            resource_item_iterator& operator++()
+            {
+                ++index;
+                return *this;
+            }
+
+            resource_item operator*() const
+            {
+                return { pool->m_dense_handles[index], pool->m_dense[index] };
+            }
+        };
+
+        struct items_range
+        {
+            resource_pool* pool;
+
+            resource_item_iterator begin()
+            {
+                return {pool, 0};
+            }
+
+            resource_item_iterator end()
+            {
+                return { pool, pool->m_dense.size() };
+            }
+        };
+
+        items_range items() { return { this }; }*/
 
         template<typename Fn>
-        void for_each(Fn&& function)
+        void for_each(Fn&& fun)
         {
-            for (auto& it : m_storage) {
-                function(it.first, it.second);
+            for (index_type i = 0; i <= m_max_idx; ++i) {
+                if (m_index_allocator.allocated(i)) {
+                    handle_t h = make_handle(i);
+                    T&       res = m_resources[i];
+                    fun(h, res);
+                }
             }
         }
 
-        constexpr auto size() const noexcept
+        size_t size() const noexcept
         {
-            return m_storage.size();
+            return m_size;
         }
 
-        T* try_get(handle_t handle)
+        T* try_get(handle_t h)
         {
-            if (m_last_handle == handle) {
-                return m_last;
+            auto idx = extract_index(h);
+            TAV_ASSERT(idx < m_index_allocator.max_index());
+
+            if (idx >= m_index_allocator.max_index()) {
+                return nullptr;
             }
-            auto it = m_storage.find(handle);
-            if (it != m_storage.end()) {
-                m_last_handle = handle;
-                m_last = &it->second;
-                return m_last;
+
+            auto gen = extract_gen(h);
+            auto cur = current_gen(idx);
+
+            if (gen != cur || !m_index_allocator.allocated(idx)) {
+                return nullptr;
             }
-            return nullptr;
+
+            return m_resources + idx;
         }
 
-        const T* try_get(handle_t handle) const
+        const T* try_get(handle_t h) const
         {
-            return try_get(handle);
+            return try_get(h);
         }
 
-        [[nodiscard]] handle_t insert(T&& res)
+        [[nodiscard]] handle_t add(T&& res)
         {
-            auto handle = m_counter.fetch_add(1);
-            m_storage[handle] = std::move(res);
-            reset_last_cache();
-            return handle;
+            auto idx = m_index_allocator.allocate();
+            TAV_ASSERT(idx != invalid_index);
+
+            if (idx == invalid_index) {
+                return invalid_handle;
+            }
+
+            // Should be called before update m_max_idx
+            ensure_allocation(idx);
+
+            if (m_max_idx < idx) {
+                m_max_idx = idx;
+            }
+
+            ++m_size;
+            m_resources[idx] = std::move(res);
+            return make_handle(idx);
         }
 
         template<typename... Args>
-        [[nodiscard]] handle_t emplace(Args&&... args)
+        [[nodiscard]] handle_t emplace_add(Args&&... args)
         {
-            auto handle = m_counter.fetch_add(1);
-            m_storage.emplace(std::piecewise_construct, std::forward_as_tuple(handle), std::forward_as_tuple(std::forward<Args>(args)...));
-            reset_last_cache();
-            return handle;
+            auto idx = m_index_allocator.allocate();
+            TAV_ASSERT(idx != invalid_index);
+
+            if (idx == invalid_index) {
+                return invalid_handle;
+            }
+
+            // Should be called before update m_max_idx
+            ensure_allocation(idx);
+
+            if (m_max_idx < idx) {
+                m_max_idx = idx;
+            }
+
+            ++m_size;
+            new (m_resources + idx) T(std::forward<Args>(args)...);
+            return make_handle(idx);
         }
 
-        void remove(handle_t handle)
+        bool erase(handle_t h)
         {
-            m_storage.erase(handle);
-            reset_last_cache();
+            auto idx = extract_index(h);
+            auto gen = extract_gen(h);
+            auto cur = current_gen(idx);
+
+            TAV_ASSERT(gen == cur);
+
+            if (gen != cur) {
+                return false;
+            }
+
+            auto success = m_index_allocator.try_deallocate(idx);
+            if (!success) {
+                return false;
+            }
+
+            increase_gen(idx);
+            m_resources[idx].~T();
+            --m_size;
+
+            return true;
         }
 
         void clear()
         {
-            m_storage.clear();
-            reset_last_cache();
+            for (index_type i = 0; i <= m_max_idx; ++i) {
+                if (m_index_allocator.allocated(i)) {
+                    m_resources[i].~T();
+                }
+            }
+            std::fill(m_gen, m_gen + idx_alc_t::k_max_index, 1);
+            m_max_idx = 0;
+            m_index_allocator.reset();
+            m_size = 0;
         }
 
     private:
-        void reset_last_cache()
+        void ensure_allocation(index_type idx)
         {
-            m_last_handle = 0;
-            m_last = nullptr;
+            auto needed_capacity = adapt_capacity(static_cast<size_t>(idx) + 1);
+            if (m_current_capacity < needed_capacity) {
+                size_t required_size = needed_capacity * sizeof(T);
+                T*     new_res = reinterpret_cast<T*>(m_allocator->allocate(required_size, alignof(T), "resource_pool"));
+
+                if (m_resources != nullptr) {
+                    move_resources_to_new_memory(new_res);
+                    m_allocator->deallocate(reinterpret_cast<uint8*>(m_resources));
+                }
+
+                m_resources = new_res;
+                m_current_capacity = needed_capacity;
+            }
+        }
+
+        size_t adapt_capacity(size_t capacity) noexcept
+        {
+            if (capacity < 2) {
+                return 2;
+            }
+            size_t adapted = static_cast<size_t>(math::ceil_power_of_two(capacity));
+            size_t max_idx = static_cast<size_t>(m_index_allocator.max_index());
+
+            if (adapted > max_idx) {
+                return max_idx;
+            }
+            return adapted;
+        }
+
+        void move_resources_to_new_memory(T* new_resources)
+        {
+            for (index_type i = 0; i <= m_max_idx; ++i) {
+                if (m_index_allocator.allocated(i)) {
+                    // Move to new resource
+                    new (new_resources + i) T(std::move(m_resources[i]));
+                    // Deallocate old resource
+                    m_resources[i].~T();
+                }
+            }
+        }
+
+        handle_t make_handle(index_type idx)
+        {
+            uint32 gen = static_cast<uint32>(current_gen(idx));
+            return (gen << 24) | idx;
+        }
+
+        uint8 current_gen(index_type idx)
+        {
+            return m_gen[idx] & 0x7f;
+        }
+
+        void increase_gen(index_type idx)
+        {
+            ++m_gen[idx];
+        }
+
+        static index_type extract_index(handle_t h)
+        {
+            return h & 0x00ffffff;
+        }
+
+        static uint8 extract_gen(handle_t h)
+        {
+            return static_cast<uint8>((h >> 24) & 0x7f);
         }
 
     private:
-        std::atomic<handle_t>            m_counter = 1;
-        core::unordered_map<handle_t, T> m_storage;
-        handle_t                         m_last_handle = 0;
-        T*                               m_last = nullptr;
+        allocator* m_allocator;
+        idx_alc_t  m_index_allocator;
+        uint8      m_gen[idx_alc_t::k_max_index]; // generation
+
+        index_type m_max_idx = 0;
+
+        T*     m_resources = nullptr;
+        size_t m_current_capacity = 0; // current capacity of m_resources
+        size_t m_size = 0;
     };
 
 } // namespace tavros::core
